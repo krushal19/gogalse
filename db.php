8ed5eefe3c94516ccc2305db9526c6bd1c370baf
krushal19 <krushaljoshi9@gmail.com>
2025-05-14 14:11:32 +0530

    db3

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$host = "127.0.0.1";        // or you can also use "localhost"
-$dbname = "gogalse";        // your database name from phpMyAdmin
-$user = "root";             // default user in XAMPP/WAMP
-$password = "";             // default password is empty
+$host = "127.0.0.1";  // or use "localhost"
+$dbname = "gogalse";
+$user = "root";
+$password = "";       // Default is blank for XAMPP
 
 try {
-    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8mb4", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch (PDOException $e) {
