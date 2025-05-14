448b064e63174fe0912adcc08ee9650196fa8288
krushal19 <krushaljoshi9@gmail.com>
2025-05-14 11:15:57 +0530

    hostname

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+$host = "gogalse.onrender.com"; // Caution: only works if MySQL is installed and listening
+$dbname = "gogalse";
+$user = "root";
+$password = ""; // Must match the DB user's password
+
+try {
+    $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected successfully";
+} catch (PDOException $e) {
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
