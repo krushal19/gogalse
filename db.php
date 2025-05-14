8e26d0cfadf72ea48bfba861672c2add0d40158b
krushal19 <krushaljoshi9@gmail.com>
2025-05-14 14:05:05 +0530

    db2

--- db.php
+++ db.php
@@ -1,11 +1,11 @@
 <?php
-$host = "https://gogalse.onrender.com/shop.php"; // Caution: only works if MySQL is installed and listening
-$dbname = "gogalse";
-$user = "root";
-$password = ""; // Must match the DB user's password
+$host = "127.0.0.1";        // or you can also use "localhost"
+$dbname = "gogalse";        // your database name from phpMyAdmin
+$user = "root";             // default user in XAMPP/WAMP
+$password = "";             // default password is empty
 
 try {
-    $conn = new PDO("mysql:host=$host;port=3306;dbname=$dbname;charset=utf8mb4", $user, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     echo "Connected successfully";
 } catch (PDOException $e) {
